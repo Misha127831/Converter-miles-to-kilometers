a00094a1869d63b193e87784b01c7b584ebae21c
MishaD <misha127831@gmail.com>
2025-04-10 09:30:55 +0300

    Add functionality to convert miles to kilometers

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,8 +2,16 @@ package app;
 
 public class Main {
 
+    private  static final double CONV_K = 1.60934;
+
     public static void main(String[] args){
         System.out.println("App to convert miles to kilometers.");
+        double mil = 7;
+        double kilometers = convMilesToKilometers(mil);
+        System.out.println("Result is " + kilometers + " km.");
+    }
 
+    private static double convMilesToKilometers(double mil){
+        return mil * CONV_K;
     }
 }
